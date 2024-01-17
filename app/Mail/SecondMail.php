c9ff1d37e6a113ed36dcd74b214e7e99299a6810
prabath1998 <prabhathudayanga1998@gmail.com>
2024-01-17 21:51:21 +0530

    send markdown email

--- app/Mail/SecondMail.php
+++ app/Mail/SecondMail.php
@@ -5,6 +5,7 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Attachment;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
@@ -51,6 +52,9 @@ class SecondMail extends Mailable
      */
     public function attachments(): array
     {
-        return [];
+        return [
+            Attachment::fromStorageDisk('public','pic.jpeg')
+            ->as('bird.jpeg'),
+        ];
     }
 }
