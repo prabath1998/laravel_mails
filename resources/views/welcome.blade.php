e5d265bb558d23d45fc25f5b944724157fea4939
prabath1998 <prabhathudayanga1998@gmail.com>
2024-01-17 20:51:56 +0530

    send first mail

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+
+
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="antialiased">
+
+    <div class="max-w-6xl mx-auto mt-12">
+        <a href="/send-mail" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded-md">Send mail</a>
+    </div>
+
+</body>
+
+</html>
